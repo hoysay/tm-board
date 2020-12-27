72d5d22386b4d2bc879601afcb04ef608074bf85
christinaou <christinaou@cmu.edu>
2020-12-27 15:34:20 -0800

    Read and write to local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+const persistedState =
+    localStorage.getItem('reduxState')
+        ? JSON.parse(localStorage.getItem('reduxState') || "")
+        : {};
+
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(()=>{
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
+
+export default store;
